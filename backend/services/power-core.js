20e8c8e127d5e6f49921e4bc9ab33b817242eab9
Jeroen van Erp <jeroen.vanerp@suse.com>
2025-03-26 12:08:23 +0100

    Fix backend vulns

--- backend/services/power-core.js
+++ backend/services/power-core.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const dns = require('dns');
+const dns = require('node:dns');
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 
